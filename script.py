989bc5e60c76550ef263acf6cb0d27c7d063b2c1
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2022-02-13 23:03:22 -0500

    updated ALU

--- script.py
+++ script.py
@@ -32,14 +32,30 @@ def full_adder(a, b, c):
     #print("s: {}, c:{}".format(new_s, new_c))
 
 
-def ALU(a, b, c, opcode):
+def ALU(a, b, c, opcode, add, subtract):
+    if add == 0:
+        pass 
+    else:
+        a = half_adder(a, add)[0]
+    if subtract == 0:
+        pass
+    else:
+        a = NAND_gate(a, add)
+
     if opcode == 0:
         s = half_adder(a, b)[0]
         c = half_adder(a, b)[-1]
     elif opcode == 1:
         s = full_adder(a, b, c)[0]
         c = full_adder(a, b, c)[-1]
+    elif opcode == 2:
+        s = full_adder(a, b, c)[0]
+        c = half_adder(a, b)[-1]
+    elif opcode == 3:
+        s = half_adder(a, b)[0]
+        c = full_adder(a, b, c)[-1]
     return s, c
 
 
-print(ALU(1, 1, 1, 0))
+
+print(ALU(1, 1, 1, 3, 0 ,0))
